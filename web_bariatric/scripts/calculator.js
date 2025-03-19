0a8ab1a80170ac1adf61329e0d34f4b614331f64
Isa <torresosorio.isabel1@gmail.com>
2025-03-19 15:29:48 -0300

    ajustes contactanos, botones de form y otros

--- web_bariatric/scripts/calculator.js
+++ web_bariatric/scripts/calculator.js
@@ -37,15 +37,15 @@ document.addEventListener("DOMContentLoaded", function() {
         let mensaje = `<p class='text-primary'>Tu IMC es: <strong>${imc.toFixed(2)}</strong></p>`;
 
         if (imc < 18.5) {
-            mensaje += "<p class='text-warning'>Bajo peso. Consulta con un especialista.</p>";
+            mensaje += "<p class='text-warning'>Tú IMC indica Bajo peso. Es necesario que acudas con un especialista.</p>";
         } else if (imc >= 18.5 && imc < 24.9) {
-            mensaje += "<p class='text-success'>Peso normal. ¡Sigue así!</p>";
+            mensaje += "<p class='text-success'>Tú IMC indica Peso normal. ¡Sigue así!</p>";
         } else if (imc >= 25 && imc < 29.9) {
-            mensaje += "<p class='text-warning'>Sobrepeso. Es recomendable un cambio en el estilo de vida.</p>";
+            mensaje += "<p class='text-warning'>Tú IMC indica Sobrepeso, Es recomendable hacer cambios en el estilo de vida.<br> En Bariatric te entregamos asesoria sobre el <strong>Balón Intragástrico Spatz </strong> y el <strong>Método Clinifit</strong>, ideales para tú IMC.</p>";
         } else if (imc >= 30 && imc < 39.9) {
-            mensaje += "<p class='text-danger'>Obesidad. Consulta a un especialista para mejorar tu salud.</p>";
+            mensaje += "<p class='text-danger'>Tú IMC indica Obesidad, Debes Consultar un especialista para mejorar tu salud. En Bariatric te entregamos asesoria sobre los diferentes procedimientos que estan disponibles para tu rango de IMC, recuerda que nuestros procedimientos van desde el balon intragástrico Spatz, hasta las cirugías bariatricas de mayor complejidad. Todas ellas aseguran un cambio positivo en tu calidad de vida. asesorate con nostros sobre los valores por procedimiento, uso de bono pad, copago de Fonasa y todas las dudas que puedas tener</p>";
         } else {
-            mensaje += "<p class='text-danger'>Obesidad mórbida. Consulta opciones de tratamiento especializado.</p>";
+            mensaje += "<p class='text-danger'>Tú Índice de Masa Corporal(IMC) indica Obesidad mórbida. Consulta las opciones de tratamiento especializado para ti, además nuestros asesores te entregarán toda la información sobre el <strong>Bono PAD de Fonasa</strong>, ya que tu IMC es uno de los requisitos de este beneficio.</p>";
         }
 
         resultadoIMC.innerHTML = mensaje;
@@ -64,7 +64,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Función para redirigir al formulario del CRM
     window.solicitarInfo = function() {
-        let formWindow = window.open("https://link.admente.com/widget/form/oX2Uxusp4DDz2dcaaZhN?notrack=true", "_blank");
+        let formWindow = window.open("https://link.admente.com/widget/form/B01j4Xmb0hw1PC7ro89C", "_blank");
 
         // Monitorear si el usuario cierra la pestaña del formulario
         let checkWindow = setInterval(function() {
